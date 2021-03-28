0c56ecfa9fed1f37ec20dd38a6bf89bbefe47684
Aya <shansharova_ayazhan@mail.ru>
2021-03-28 23:38:02 +0600

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import h from"./Header.module.css"
+
+const Header = ()=>{
+    return(
+        <header className = {h.header}>
+        <img src="https://static.rfstat.com/renderforest/images/v2/logo-homepage/flat_3.png"/>
+      </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import classes from "./Nav.module.css"
+
+const Nav = ()=>{
+    return(
+        <nav className={classes.nav}>
+            <div className = {classes.item}><a>Profile</a></div>
+            <div className = {classes.item}><a>News</a></div>
+            <div className = {classes.item}><a>Message</a></div>
+            <div className = {classes.item}><a>Music</a></div>
+            <div className = {classes.item}><a>Settings</a></div>
+        </nav>
+    )
+}
+
+export default Nav;
