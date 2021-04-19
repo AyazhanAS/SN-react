ec3c0f765baad0525d5a09910f0040d200b08179
Aya <shansharova_ayazhan@mail.ru>
2021-04-19 12:46:44 +0600

    20-th lesson

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import Content from './components/profile/Content';
 import Nav from './components/navbar/Nav';
 import Header from "./components/header/Header"
 import Dialogs from './components/dialogs/Dialogs';
+import { Route } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
+import News from "./components/news/News";
+import Music from "./components/music/Music";
+import Settings from "./components/settings/Settings";
 
 function App() {
   return (
-    <div className="App-wrapper">
-      <Header/>
-      <Nav/>
-      <div className="App-wrapper-var">
-      <Content/>
-      </div>
-      
-      {/* <Content/> */}
-    </div>
+    <BrowserRouter>
+       <div className="App-wrapper">
+          <Header/>
+          <Nav/>
+          <div className="App-wrapper-var">
+            <Route path="/profile" component={Content}/>
+            <Route path="/dialogs" component={Dialogs}/>
+            <Route path="/news" component={News}/>
+            <Route path="/music" component={Music} />
+            <Route path="/settings" component={Settings} />
+          </div>
+       </div>
+    </BrowserRouter>
+ 
   );
 }
 
