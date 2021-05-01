fe03e0dc85e79dd7e4a1dceeb1612d01768e4021
Aya <shansharova_ayazhan@mail.ru>
2021-05-01 18:03:37 +0600

    51-th lesson

--- src/components/users/Users.jsx
+++ src/components/users/Users.jsx
@@ -1,18 +1,20 @@
+import axios from 'axios'
 import React from 'react'
 import styles from "./Users.module.css"
+import UserPhoto from "../../assets/user.png"
 
 export default function Users(props) {
 
     if(!props.users.length){
-        props.setUser(
-            [
-              {id: 1, followed: false, fulName: "Batosh", photoURL:"https://img-fotki.yandex.ru/get/52765/340977384.1/0_13c56d_e8a81eaf_orig",  status: "I am a Boss", location: {country: "Kazakhstan", city: "Almaty"}},
-              {id: 2, followed: false, fulName: "Kabluda", photoURL:"https://img-fotki.yandex.ru/get/52765/340977384.1/0_13c56d_e8a81eaf_orig", status: "I am a berry", location: {country: "Kazakhstan", city: "Nursultan"}},
-              {id: 3, followed: true, fulName: "SpangeBob", photoURL:"https://img-fotki.yandex.ru/get/52765/340977384.1/0_13c56d_e8a81eaf_orig",  status: "I am a personaj", location: {country: "USA", city: "Chikago"}},
-              {id: 4, followed: true, fulName: "Babay", photoURL:"https://img-fotki.yandex.ru/get/52765/340977384.1/0_13c56d_e8a81eaf_orig", status: "I am a Babay", location: {country: "Kazakhstan", city: "BabayCity"}}
-               
-              ]
+
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response =>{
+    
+            props.setUser(response.data.items)
+        }
         )
+
+     
 
     }
 
@@ -21,7 +23,7 @@ export default function Users(props) {
             {props.users.map(u => <div key = {u.id}>
                 <span>
                     <div>
-                        <img alt="" className = {styles.imgUsers} src={u.photoURL}/>
+                        <img alt="" className = {styles.imgUsers} src={u.photos.small!=null ? u.photos.small: UserPhoto}/>
                     </div>
                     <div>
                         {u.followed  
@@ -33,15 +35,15 @@ export default function Users(props) {
                 <span>
                     <span>
                         <div>
-                            {u.fullName}
+                            {u.name}
                         </div>
                         <div>
                             {u.status}
                         </div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>               
+                        <div>{"u.location.country"}</div>
+                        <div>{"u.location.city"}</div>               
                     </span>
                 </span>
             </div>)}
